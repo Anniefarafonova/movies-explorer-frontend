562f9f8d7421ed2687473c5ae41fe5d9e41ceb2c
Anna Farafonova <annie.hop@yandex.ru>
2023-11-08 09:53:32 +0300

    test

--- src/components/MoviesCard/MoviesCard.js
+++ src/components/MoviesCard/MoviesCard.js
@@ -6,14 +6,14 @@ import MainApi from "../../utils/MainApi";
 import { CurrentUserContext } from "../../contexts/CurrentUserContextt/CurrentUserContext.js"
 
 
-export default function MoviesCard({ movie, filterMovies, savedMovies,  setSavedMovies, handleAddSubmit, handleDeleteSubmit, isSaved}) {
+export default function MoviesCard({ movie, filterMovies, savedMovies, setSavedMovies, handleAddSubmit, handleDeleteSubmit, isSaved }) {
     const [isLike, setIsLike] = React.useState(false);
     useEffect(() => {
         const isLiked = savedMovies.some((element => movie.id === element.movieId));
         setIsLike(isLiked);
-      }, [savedMovies]);
+    }, [savedMovies]);
 
-      function onClick() {
+    function onClick() {
         if (savedMovies.some(element => movie.id === element.movieId)) {
             setIsLike(false)
             handleAddSubmit(movie)
@@ -22,44 +22,45 @@ export default function MoviesCard({ movie, filterMovies, savedMovies,  setSaved
             handleAddSubmit(movie)
         }
         setIsLike(!isLike);
-      }
-       
+    }
+
     return (
         <>
             <article className="element">
-                <img className="element__image" alt="" 
-                src={ 
-                    !isSaved ?
-                    `https://api.nomoreparties.co${movie.image.url}`:
-                    movie.image
-                } />
+                <Link to={movie.trailerLink} target='_blank'>
+                <img className="element__image" alt=""
+                    src={
+                        !isSaved ?
+                            `https://api.nomoreparties.co${movie.image.url}` :
+                            movie.image
+                    } />
+            </Link>
 
-                
-                <ul className="element__list">
-                    <li className="element__list-self">
-                        <h2 className="element__list-title">{movie.nameRU}</h2>
-                        {isSaved ? (
-                            <button className="element__list-dislike" 
+            <ul className="element__list">
+                <li className="element__list-self">
+                    <h2 className="element__list-title">{movie.nameRU}</h2>
+                    {isSaved ? (
+                        <button className="element__list-dislike"
                             onClick={() => handleDeleteSubmit(movie._id)}
-                            />
-                        ) : (
-                            <button
-                                className={
-                                    isLike
-                                        ? `element__list-like_active`
-                                        : `element__list-like`
-                                }
-                                type="button"
-                                aria-label="Значок лайк"
-                                onClick={onClick}
-                            />
-                        )}
-                    </li>
-                    <li className="element__list-item">
-                        <p className="element__list-time">{movie.duration}</p>
-                    </li>
-                </ul>
-            </article>
+                        />
+                    ) : (
+                        <button
+                            className={
+                                isLike
+                                    ? `element__list-like_active`
+                                    : `element__list-like`
+                            }
+                            type="button"
+                            aria-label="Значок лайк"
+                            onClick={onClick}
+                        />
+                    )}
+                </li>
+                <li className="element__list-item">
+                    <p className="element__list-time">{movie.duration}</p>
+                </li>
+            </ul>
+        </article >
 
         </>
     )
